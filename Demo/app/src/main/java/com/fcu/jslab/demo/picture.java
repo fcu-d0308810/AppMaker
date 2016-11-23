61268b1f7650ab7553d44a5fcf84f0a3cf70c405
Lo 4J <ihaveadream0528@yahoo.com.tw>
2016-11-23 22:25:50 +0800

    增加圖片上傳java檔

--- Demo/app/src/main/java/com/fcu/jslab/demo/picture.java
+++ Demo/app/src/main/java/com/fcu/jslab/demo/picture.java
@@ -2,13 +2,27 @@ package com.fcu.jslab.demo;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class picture extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +32,7 @@ public class picture extends AppCompatActivity implements View.OnClickListener {
     private ImageView imageView;
     private TextView message_text;
     private ProgressDialog dialog = null;
+    private int serverResponseCode = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,5 +59,129 @@ public class picture extends AppCompatActivity implements View.OnClickListener {
             //dialog = ProgressDialog.show();
         }
     }
+    public int uploadFile(String sourceFileUri){
+        String filename = sourceFileUri;
+        HttpURLConnection connection = null;
+        DataOutputStream dos = null;
+        File sourceFile = new File(sourceFileUri);
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        if(!sourceFile.isFile()){
+            dialog.dismiss();
+            Log.e("uploadFile","Source File is not exist: " + imagepath);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    message_text.setText("Source File is not exist: " + imagepath);
+                }
+            });
+            return 0;
+        }
+        else{
+            try{
+
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(uploadSeverURL);
+                //打開httpconnection
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setUseCaches(false);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file", filename);
+
+                dos = new DataOutputStream(connection.getOutputStream());
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + filename + "\"" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                buffer = new byte[bufferSize];
+                bytesRead = fileInputStream.read(buffer, 0,bufferSize);
+                while(bytesRead > 0){
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                serverResponseCode = connection.getResponseCode();
+                String serverResponseMessage = connection.getResponseMessage();
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            String msg = "File Upload Completed.\n\n See uploaded file your server. \n\n";
+                            message_text.setText(msg);
+                            Toast.makeText(picture.this, "File Upload Complete.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            }catch (MalformedURLException ex){
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        message_text.setText("MalformedURLException Exception : check script url.");
+                        Toast.makeText(picture.this, "MalformedURLException", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            }catch (Exception e){
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        message_text.setText("Got Exception : see logcat ");
+                        Toast.makeText(picture.this, "Got Exception : see logcat ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file Exception", "Exception : "  + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            //Bitmap photo = (Bitmap) data.getData().getPath();
+
+            Uri selectedImageUri = data.getData();
+            imagepath = getPath(selectedImageUri);
+            Bitmap bitmap= BitmapFactory.decodeFile(imagepath);
+            imageView.setImageBitmap(bitmap);
+            message_text.setText("Uploading file path:" +imagepath);
+
+        }
+    }
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
 
 }
--- Demo/app/src/main/java/com/fcu/jslab/demo/picture_and_video.java
+++ Demo/app/src/main/java/com/fcu/jslab/demo/picture_and_video.java
@@ -1,6 +1,7 @@
 package com.fcu.jslab.demo;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +15,27 @@ import android.widget.TextView;
  */
 
 public class picture_and_video extends Fragment{
+    Button picture_button, video_button;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
-        View view = inflater.inflate(R.layout.messageto, container, false);
+        View view = inflater.inflate(R.layout.picture_and_video, container, false);
+        picture_button = (Button) view.findViewById(R.id.Picture_Button);
+        video_button = (Button) view.findViewById(R.id.Video_Button);
+        picture_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(),picture.class);
+                startActivity(intent);
+            }
+        });
+        video_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(),picture.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
